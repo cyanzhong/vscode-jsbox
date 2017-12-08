3cfa22640fa1c7dc519a054573d9e3431bdac284
cyan <0x00eeee@gmail.com>
2017-12-09 00:21:21 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# vscode-jsbox
-JSBox Development Utilities on VSCode
+# JSBox
+
+JSBox Development Utilities on VSCode!
+
+This is a VSCode extension for [JSBox](https://docs.xteko.com), it provides useful features like `syntax highlighting`, `auto completion` etc.
+
+The most important is, when you save a script on VSCode, it runs on your iPhone automatically!
+
+## Extension Settings
+
+On iPhone, turn on debug mode of JSBox and restart the app, you can see the `Host` in the settings view.
+
+On VSCode, you need to set `host`:
+
+* `jsbox.host`: web server host
+
+There are also two super easy ways to setup host:
+
+- Click menu button at the top-right corner of your editor panel, there's a `Set Host` item
+- Trigger VSCode command with `command+shift+p`, type sethost then execute the command
+
+## Contacts
+
+- [GitHub](https://github.com/cyanzhong/vscode-jsbox)
+- [Docs](https://docs.xteko.com)
+- [Email](mailto:log.e@qq.com)
+
+## Release Notes
+
+### 0.1.0
+
+Initial release
---
+++ src/extension.ts
@@ -0,0 +1,57 @@
+'use strict';
+
+import {commands, workspace, window, ExtensionContext} from 'vscode';
+
+// Extension activate
+export function activate(context: ExtensionContext) {
+
+  // Observe command to setup host
+  context.subscriptions.push(commands.registerCommand('extension.setHost', setHost));
+
+  // Observe file changes
+  let path = window.activeTextEditor.document.fileName;
+  let watcher = workspace.createFileSystemWatcher(path);
+  watcher.onDidChange(syncFile);
+}
+
+// Configure the host
+function setHost() {
+  window.showInputBox({
+    placeHolder: 'Example: 10.106.144.196',
+    value: workspace.getConfiguration().host
+  }).then((value) => {
+    if (value && value.length > 0) {
+      workspace.getConfiguration().update('host', value, true);
+      window.showInformationMessage('Host: ' + value);
+    }
+  });
+}
+
+// Show error message
+function onError(error) {
+  console.error(error);
+  window.showErrorMessage(error);
+}
+
+// Sync file
+function syncFile() {
+
+  // Check host is available
+  let host = workspace.getConfiguration().host;
+  
+  if (host.length === 0) {
+    onError('Host is unavailable');
+    return;
+  }
+  
+  // Upload file to server
+  let path = window.activeTextEditor.document.fileName;
+  var request = require('request');
+  var fs = require('fs');
+  var formData = {'files[]': fs.createReadStream(path)};
+  request.post({url: 'http://' + host + '/upload', formData: formData}, (error) => {
+    if (error) {
+      onError(error);
+    }
+  });
+}
